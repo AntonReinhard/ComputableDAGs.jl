587b03eed000c04029f917e3e07abf2d2bf184d4
Anton Reinhard <anton.reinhard@proton.me>
2025-01-23 15:18:34 +0100

    WIP

--- benchmark/qed_scaling.jl
+++ benchmark/qed_scaling.jl
@@ -1,7 +1,9 @@
+using Distributed
 using ComputableDAGs
+using Pkg
+Pkg.develop(; path="/home/reinha57/repos/QEDFeynman.jl/")
 using QEDFeynman
 using RuntimeGeneratedFunctions
-using Plots
 using DataFrames
 using BenchmarkTools
 using QEDcore, QEDprocesses
@@ -61,6 +63,33 @@ function profile(instance, input, optimizer, closures_size)
 end
 =#
 
+function time_compilation(expr; setup=nothing)
+    ps = addprocs(1)
+    remotecall_fetch(only(ps)) do
+        @eval begin
+            using QEDprocesses, QEDcore, ComputableDAGs, QEDFeynman
+        end
+    end
+
+    (; compile_time) = remotecall_fetch(only(ps)) do
+        @eval begin
+            $setup
+            @timed $expr
+        end
+    end
+    rmprocs(ps)
+    return compile_time
+end
+
+function bench_compilation(expr; setup=nothing, n=20)
+    times = Float64[]
+    for _ in 1:n
+        push!(times, time_compilation(expr; setup=setup))
+    end
+
+    return times
+end
+
 # ------------------
 
 df = DataFrame()
@@ -68,37 +97,40 @@ df = DataFrame()
 MODEL = PerturbativeQED()
 
 SCATTERING_PROCESSES = [
-    "ke->ke",
-    "kke->ke",
-    "kkke->ke",
-    "kkkke->ke",
-    "kkkkke->ke",
-    "kkkkkke->ke",
-    "kkkkkkke->ke",
+    "ke->ke",               # 1
+    "kke->ke",              # 2
+    "kkke->ke",             # 3
+    "kkkke->ke",            # 4
+    "kkkkke->ke",           # 5
+    "kkkkkke->ke",          # 6
+    "kkkkkkke->ke",         # 7
+    "kkkkkkkke->ke",        # 8
+    #"kkkkkkkkke->ke",       # 9
 ]
 
 SUITE = BenchmarkGroup()
 SUITE["graph_gen"] = BenchmarkGroup()
 
 graph_props = Dict{String,GraphProperties}()
+comp_times = Dict{String,Vector{Float64}}()
+node_dicts = Dict{String,Dict{Type,Int64}}()
 
 for INSTANCE_STR in SCATTERING_PROCESSES
     INSTANCE = parse_process(INSTANCE_STR, QEDModel())
     println("$INSTANCE_STR")
     graph(INSTANCE)
-    SUITE["graph_gen"][INSTANCE_STR] = @benchmarkable graph(proc) setup = (
-        proc = $INSTANCE; GC.gc()
-    )
+    SUITE["graph_gen"][INSTANCE_STR] = @benchmarkable graph(proc) setup = (proc = $INSTANCE; GC.gc())
 
     g = graph(INSTANCE)
     graph_props[INSTANCE_STR] = get_properties(g)
 
-    SUITE["f_gen"][INSTANCE_STR] = @benchmarkable get_compute_function(
-        g_, proc, machine, @__MODULE__; closures_size=0
-    ) setup = (g_ = $g; proc = $INSTANCE; machine = cpu_st(); GC.gc())
-
-    if graph_props[INSTANCE_STR].number_of_nodes > 30000
-        continue
+    node_dicts[INSTANCE_STR] = Dict{Type,Int64}()
+    for node in g.nodes
+        if haskey(node_dicts[INSTANCE_STR], typeof(task(node)))
+            node_dicts[INSTANCE_STR][typeof(task(node))] = node_dicts[INSTANCE_STR][typeof(task(node))] + 1
+        else
+            node_dicts[INSTANCE_STR][typeof(task(node))] = 1
+        end
     end
 
     psp = PhaseSpacePoint(
@@ -111,13 +143,35 @@ for INSTANCE_STR in SCATTERING_PROCESSES
 
     func = get_compute_function(g, INSTANCE, cpu_st(), @__MODULE__; closures_size=0)
 
-    SUITE["f_exec"][INSTANCE_STR] = @benchmarkable f(input) setup = (
-        f = $func; input = $psp; GC.gc()
+    SUITE["f_gen"][INSTANCE_STR] = @benchmarkable get_compute_function(g_, proc, machine, @__MODULE__; closures_size=0) setup = (
+        g_ = $g; proc = $INSTANCE; machine = cpu_st(); GC.gc()
     )
+
+    if graph_props[INSTANCE_STR].number_of_nodes > 30000
+        continue
+    end
+
+    comp_times[INSTANCE_STR] = bench_compilation(
+        :(f(p));
+        setup=quote
+            using QEDcore, QEDprocesses, RuntimeGeneratedFunctions
+            RuntimeGeneratedFunctions.init(@__MODULE__)
+            p = PhaseSpacePoint(
+                $INSTANCE,
+                $MODEL,
+                PhasespaceDefinition(SphericalCoordinateSystem(), ElectronRestFrame()),
+                tuple((rand(SFourMomentum) for _ in 1:number_incoming_particles($INSTANCE))...),
+                tuple((rand(SFourMomentum) for _ in 1:number_outgoing_particles($INSTANCE))...),
+            )
+            f = get_compute_function($g, $INSTANCE, cpu_st(), @__MODULE__; closures_size=0)
+        end,
+    )
+    println("collected $(length(comp_times[INSTANCE_STR])) compile time samples")
+    SUITE["f_exec"][INSTANCE_STR] = @benchmarkable f(input) setup = (f = $func; input = $psp; GC.gc())
 end
 
 tune!(SUITE)
 result = run(SUITE; verbose=true)
 
 BenchmarkTools.save("bench.json", result)
-@save "bench.jld2" result graph_props
+@save "bench.jld2" result graph_props node_dicts comp_times
