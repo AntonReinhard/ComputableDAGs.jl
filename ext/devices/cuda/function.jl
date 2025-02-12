589e88d623c5c5cb4d30b97c526208dd2516bb20
Anton Reinhard <anton.reinhard@proton.me>
2025-02-12 14:14:04 +0100

    New runs and notebook

--- ext/devices/cuda/function.jl
+++ ext/devices/cuda/function.jl
@@ -1,6 +1,4 @@
-function ComputableDAGs.kernel(
-    ::Type{CUDAGPU}, graph::DAG, instance, context_module::Module
-)
+function ComputableDAGs.kernel(::Type{CUDAGPU}, graph::DAG, instance, context_module::Module)
     machine = cpu_st()
     tape = ComputableDAGs.gen_tape(graph, instance, machine, context_module)
 
@@ -9,19 +7,21 @@ function ComputableDAGs.kernel(
     code = Expr(:block, ComputableDAGs.expr_from_fc.(tape.schedule)...)
 
     function_id = ComputableDAGs.to_var_name(UUIDs.uuid1(ComputableDAGs.rng[1]))
+    #!format: off
     expr = Meta.parse(
         "function compute_$(function_id)(input_vector, output_vector, n::Int64)
             id = (blockIdx().x - 1) * blockDim().x + threadIdx().x
             if (id > n)  
                 return
             end
-            @inline input = input_vector[id]
+            @inbounds input = input_vector[id]
             $(assign_inputs)
             $code
-            @inline output_vector[id] = $(tape.output_symbol)
+            @inbounds output_vector[id] = $(tape.output_symbol)
             return nothing
         end"
     )
+    #!format: on
 
     return expr
 end
