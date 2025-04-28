eb1d59f11d8a3513c72e6a8ba6300c9dc10164f6
Anton Reinhard <anton.reinhard@proton.me>
2025-04-28 13:49:01 +0200

    Add speedup plots

--- experiments/04_optimization/plotting/opt_progress_speedup.jl
+++ experiments/04_optimization/plotting/opt_progress_speedup.jl
@@ -1,4 +1,4 @@
-# == Function Execution Time CPU/GPU==
+# == Function Speedup CPU/GPU==
 
 using Unitful: percent
 
@@ -20,24 +20,24 @@ for PROC in SCATTERING_PROCESSES
     data_cpu = getindex.(Ref(data_cpu), STEPS)
     data_cpu = getfield.(data_cpu, :times)
     data_cpu = median.(data_cpu)
-    data_cpu = data_cpu ./ data_cpu[1]
+    data_cpu = data_cpu[1] ./ data_cpu
 
     data_gpu = getindex.(Ref(data_gpu), STEPS)
     data_gpu = getfield.(data_gpu, :times)
     data_gpu = median.(data_gpu)
     data_gpu = data_gpu ./ Ref(N)
-    data_gpu = data_gpu ./ data_gpu[1]
+    data_gpu = data_gpu[1] ./ data_gpu
 
     data_flops = getindex.(data_flops, Ref(2)) # graph properties
     data_flops = getfield.(data_flops, Ref(:compute_effort))
-    data_flops = data_flops ./ data_flops[1]
+    data_flops = data_flops[1] ./ data_flops
 
     f = Figure(; size = (600, 500))
     ax = Axis(
         f[1, 1];
         xlabel = "number of optimization steps",
-        ylabel = "normalized execution time",
-        limits = (nothing, (-0.05, 1.05)),
+        ylabel = "normalized speedup",
+        #limits = (nothing, _find_y_lims([data_cpu, data_gpu, data_flops])),
         yminorgridvisible = true,
         yminorticksvisible = true,
         yminorticks = IntervalsBetween(5),
@@ -45,7 +45,7 @@ for PROC in SCATTERING_PROCESSES
         xminorticks = IntervalsBetween(5),
         #yscale=log10,
         #xticks=(STEPS, string.(STEPS)),
-        yticks = ([0.0, 0.5, 1.0], ["0 %", "50 %", "100 %"]),
+        #yticks=([0.0, 0.5, 1.0], ["0 %", "50 %", "100 %"]),
     )
 
     sc_cpu = plot!(ax, STEPS, data_cpu; markersize = 12, color = colors[1])
@@ -60,5 +60,5 @@ for PROC in SCATTERING_PROCESSES
         f[2, 1], elements, labels; tellheight = true, tellwidth = false, margin = (10, 10, 10, 10), orientation = :horizontal
     )
 
-    save(joinpath(plotpath, "opt_progress_flops_norm", "opt_progress_$(proc_n(PROC)).pdf"), f)
+    save(joinpath(plotpath, "opt_progress_speedup", "opt_progress_$(proc_n(PROC)).pdf"), f)
 end
