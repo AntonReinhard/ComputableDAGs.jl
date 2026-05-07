65560c1834bfb1e2f61bb1ce5a29ed1c57b6320c
Anton Reinhard <anton.reinhard@proton.me>
2026-05-07 12:05:57 +0200

    hal benchmarking and plots

--- testbed/ipc_communication/bench.jl
+++ testbed/ipc_communication/bench.jl
@@ -2,41 +2,60 @@ using CSV
 using DataFrames
 using JLD2
 
-df = DataFrame(socket_type = String[], N = Int[], data_size = Int[], D0_times = Float64[], D1_times = Float64[], D2_times = Float64[])
+df = DataFrame(socket_type=String[], N=Int[], data_size=Int[], D0_times=Float64[], D1_times=Float64[], D2_times=Float64[], RUN=Int[])
+
+RUNS = Dict(
+    1 => 50,
+    2 => 50,
+    3 => 50,
+    4 => 50,
+    6 => 25,
+    8 => 25,
+    11 => 25,
+    16 => 10,
+    23 => 10,
+    32 => 10,
+    45 => 10,
+    64 => 10,
+    91 => 10
+)
 
 for SOCKET_TYPE in ["tcp", "ipc"], N in [round(Int, 2^n) for n in 0.5:0.5:10], DATA in [round(Int, 2^n) for n in 0:3:15]
-    d0_command = `julia example.jl -q -n $N -d 0 -s $SOCKET_TYPE --data $DATA`
-    d1_command = `julia example.jl -q -n $N -d 1 -s $SOCKET_TYPE --data $DATA`
-    d2_command = `julia example.jl -q -n $N -d 2 -s $SOCKET_TYPE --data $DATA`
+    for RUN in 1:get(RUNS, N, 5)
+        d0_command = `julia example.jl -q -n $N -d 0 -s $SOCKET_TYPE --data $DATA`
+        d1_command = `julia example.jl -q -n $N -d 1 -s $SOCKET_TYPE --data $DATA`
+        d2_command = `julia example.jl -q -n $N -d 2 -s $SOCKET_TYPE --data $DATA`
 
-    io_d0 = PipeBuffer()
-    io_d1 = PipeBuffer()
-    io_d2 = PipeBuffer()
+        io_d0 = PipeBuffer()
+        io_d1 = PipeBuffer()
+        io_d2 = PipeBuffer()
 
-    t0 = Threads.@spawn run(d0_command, devnull, io_d0, stderr)
-    t1 = Threads.@spawn run(d1_command, devnull, io_d1, stderr)
-    t2 = Threads.@spawn run(d2_command, devnull, io_d2, stderr)
+        t0 = Threads.@spawn run(d0_command, devnull, io_d0, stderr)
+        t1 = Threads.@spawn run(d1_command, devnull, io_d1, stderr)
+        t2 = Threads.@spawn run(d2_command, devnull, io_d2, stderr)
 
-    Threads.wait(t0)
-    Threads.wait(t1)
-    Threads.wait(t2)
+        Threads.wait(t0)
+        Threads.wait(t1)
+        Threads.wait(t2)
 
-    d0_time = parse(Float64, readlines(io_d0)[1])
-    d1_time = parse(Float64, readlines(io_d1)[1])
-    d2_time = parse(Float64, readlines(io_d2)[1])
+        d0_time = parse(Float64, readlines(io_d0)[1])
+        d1_time = parse(Float64, readlines(io_d1)[1])
+        d2_time = parse(Float64, readlines(io_d2)[1])
 
-    @info "Got times $d0_time/$d1_time/$d2_time for N=$N and SOCKET_TYPE $SOCKET_TYPE and $DATA B of data"
+        @info "Got times $d0_time/$d1_time/$d2_time for N=$N and SOCKET_TYPE $SOCKET_TYPE and $DATA B of data"
 
-    push!(
-        df, Dict(
-            :socket_type => SOCKET_TYPE,
-            :N => N,
-            :data_size => DATA,
-            :D0_times => d0_time,
-            :D1_times => d1_time,
-            :D2_times => d2_time
+        push!(
+            df, Dict(
+                :socket_type => SOCKET_TYPE,
+                :N => N,
+                :data_size => DATA,
+                :D0_times => d0_time,
+                :D1_times => d1_time,
+                :D2_times => d2_time,
+                :RUN => RUN
+            )
         )
-    )
+    end
 end
 
 @info "Writing to file"
--- testbed/ipc_communication/plot.jl
+++ testbed/ipc_communication/plot.jl
@@ -1,48 +1,132 @@
 using CairoMakie
 using JLD2
 using DataFrames
+using Statistics
+
+yticks_t = (
+    [1e-4, 2e-4, 5e-4, 1e-3, 2e-3, 5e-3, 1e-2, 2e-2, 5e-2, 1e-1, 2e-1, 5e-1, 1.0],
+    ["100 μs", "200 μs", "500 μs", "1 ms", "2 ms", "5 ms", "10 ms", "20 ms", "50 ms", "100 ms", "200 ms", "500 ms", "1 s"]
+)
+yticks_b = (
+    [1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10],
+    ["1 kB/s", "10 kB/s", "100 kB/s", "1 MB/s", "10 MB/s", "100 MB/s", "1 GB/s", "10 GB/s"]
+)
 
 # loads "df" DataFrame
-@load "data/bench.jld2"
+@load "data/bench_26_05_07.jld2"
 
 colors = Makie.wong_colors()
 
+RUNS = length(Set(df.RUN))
+
+# get medians of the runs
+df = groupby(df, [:socket_type, :N, :data_size])
+df = combine(
+    df,
+    :D0_times => median => :D0_times,
+    :D1_times => median => :D1_times,
+    :D2_times => median => :D2_times,
+    :D0_times => std => :D0_std,
+    :D1_times => std => :D1_std,
+    :D2_times => std => :D2_std,
+)
+
 # use set to deduplicate
 for data_size in Set(df.data_size)
-    f = Figure(; title = "Small test graph communication, sent data packet size = $data_size B")
+    @info "Plotting data size $data_size"
+    f = Figure(;)
     ax = Axis(
         f[1, 1];
-        xlabel = "number of runs (#)",
-        ylabel = "time taken (s)",
-        yminorgridvisible = true,
-        yminorticksvisible = true,
-        xscale = log10,
-        yscale = log10
+        xlabel="number of runs (#)",
+        ylabel="time taken",
+        yminorgridvisible=false,
+        yminorticksvisible=false,
+        xscale=log10,
+        yscale=log10,
+        xticks=[2^n for n in 0:10],
+        yticks=yticks_t,
+        title="Test graph communication, sent data packet size = $data_size B, 7 transfers"
     )
 
-    data = df[(df.data_size .== data_size), :]
+    data = df[(df.data_size.==data_size), :]
 
     elements = []
     labels = []
 
     for (socket_type, color) in [("tcp", 1), ("ipc", 2)]
-        relevant_data = data[(data.socket_type .== Ref(socket_type)), :]
+        relevant_data = data[(data.socket_type.==Ref(socket_type)), :]
+
         xdata = relevant_data.N
+
         y0data = relevant_data.D0_times
         y1data = relevant_data.D1_times
         y2data = relevant_data.D2_times
 
-        ydata = (y0data .+ y1data .+ y2data) ./ 3
+        y0std = relevant_data.D0_std
+        y1std = relevant_data.D1_std
+        y2std = relevant_data.D2_std
 
-        p = plot!(ax, xdata, ydata; color = colors[color], markersize = 10)
-        #plot!(ax, xdata, y1data; color=colors[color], markersize=10)
-        #plot!(ax, xdata, y2data; color=colors[color], markersize=10)
+        p = plot!(ax, xdata, y0data; color=colors[color], markersize=10, marker=:x)
+        plot!(ax, xdata, y1data; color=colors[color], markersize=10, marker=:x)
+        plot!(ax, xdata, y2data; color=colors[color], markersize=10, marker=:x)
+
+        #= doesn't work with log y axis
+        errorbars!(ax, xdata, y0data, y0std; color=colors[color], whiskerwidth=7)
+        errorbars!(ax, xdata, y1data, y1std; color=colors[color], whiskerwidth=7)
+        errorbars!(ax, xdata, y2data, y2std; color=colors[color], whiskerwidth=7)=#
 
         push!(elements, p)
         push!(labels, "Socket type \'$socket_type\'")
     end
 
-    Legend(f[1, 1], elements, labels; tellheight = false, tellwidth = false, halign = :left, valign = :top)
+    Legend(f[1, 1], elements, labels; tellheight=false, tellwidth=false, halign=:left, valign=:top)
 
-    save("plots/communication_bench_$(data_size)B.pdf", f)
+    save("plots/26_05_07/communication_bench_$(data_size)B_time.pdf", f)
+
+    f = Figure(;)
+    ax = Axis(
+        f[1, 1];
+        xlabel="number of runs (#)",
+        ylabel="data transfer rate (B/s)",
+        yminorgridvisible=false,
+        yminorticksvisible=false,
+        xscale=log10,
+        #yscale=log10,
+        xticks=[2^n for n in 0:10],
+        #yticks=yticks_b,
+        title="Test graph communication, sent data packet size = $data_size B, 7 transfers"
+    )
+
+    data = df[(df.data_size.==data_size), :]
+
+    elements = []
+    labels = []
+
+    for (socket_type, color) in [("tcp", 1), ("ipc", 2)]
+        relevant_data = data[(data.socket_type.==Ref(socket_type)), :]
+
+        xdata = relevant_data.N
+
+        # total of 7 IPC transports of size data_size each
+        y0data = data_size ./ relevant_data.D0_times .* 7 .* xdata
+        y1data = data_size ./ relevant_data.D1_times .* 7 .* xdata
+        y2data = data_size ./ relevant_data.D2_times .* 7 .* xdata
+
+        p = plot!(ax, xdata, y0data; color=colors[color], markersize=10, marker=:x)
+        plot!(ax, xdata, y1data; color=colors[color], markersize=10, marker=:x)
+        plot!(ax, xdata, y2data; color=colors[color], markersize=10, marker=:x)
+
+        #=errorbars!(ax, xdata, y0data, y0std; color=colors[color], whiskerwidth=7)
+        errorbars!(ax, xdata, y1data, y1std; color=colors[color], whiskerwidth=7)
+        errorbars!(ax, xdata, y2data, y2std; color=colors[color], whiskerwidth=7)=#
+
+        push!(elements, p)
+        push!(labels, "Socket type \'$socket_type\'")
+    end
+
+    Legend(f[1, 1], elements, labels; tellheight=false, tellwidth=false, halign=:left, valign=:top)
+
+    save("plots/26_05_07/communication_bench_$(data_size)B_datarate.pdf", f)
+
+
 end
